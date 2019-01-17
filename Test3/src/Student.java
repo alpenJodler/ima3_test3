3d1f49723f39f07d412857b0af1439a7e42874c8
meyer.lokal <meyer.lokal@172.18.128.43>
2019-01-17 09:20:10 +0100

    add javadoc

--- Test3/src/Student.java
+++ Test3/src/Student.java
@@ -1,10 +1,19 @@
-
+/**
+ * Klasse student
+ * @author meyer.lokal
+ *
+ */
 public class Student {
 	private String name;
 	private int gebJahr;
 	private String studiengang;
 	
-	
+	/**
+	 * Konstruktor 
+	 * @param name
+	 * @param gebJahr
+	 * @param studiengang
+	 */
 	public Student(String name, int gebJahr, String studiengang) {
 		super();
 		this.name = name;
@@ -12,10 +21,14 @@ public class Student {
 		this.studiengang = studiengang;
 	}
 
+	/**
+	 * Konsolenausgabe der wichtigsten Attribute
+	 */
 	public void ausgeben() {
 		System.out.println(name + " / " + studiengang);
 	}
 
+	
 	public static void main(String[] args) {
 		Student s1 = new Student("Susi", 1998, "IMA");
 		s1.ausgeben();
